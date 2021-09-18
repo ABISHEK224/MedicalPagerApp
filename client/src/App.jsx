3ee6bcb0163f495884e2e54c8ea5f88d44ff0b05
Abishek <abi.2000.rs@gmail.com>
2021-09-18 23:33:27 +0530

    Added node modules

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-
+import {StreamChat} from 'stream-chat'
+import {Chat} from 'stream-chat-react'
+import cookies from "universal-cookie"
 const App = () => {
     return (
         <div>
